e3aa3573bcb5a787543a63f2e199226c476d71f9
Damian <damian.ogorek.1996@gmail.com>
2020-03-11 22:08:53 +0100

    Added support to lowest version on API to draw vector objects

--- app/src/main/java/com/bignerdranch/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/diceroller/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.bignerdranch.android.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roll_button.text = "Let's roll"
+        roll_button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawable = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        dice_image.setImageResource(drawable)
     }
 }
